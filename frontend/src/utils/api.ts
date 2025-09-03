307f184b87c8a999c6aba681c0927c2147efc1d1
Aadil Ali <61555486+aadilaliofficial@users.noreply.github.com>
2025-09-03 18:49:09 +0530

    Update api.ts

--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "https://notes-taking-app-fxv2.onrender.com/api", // backend base url
   withCredentials: true,
 });
 
